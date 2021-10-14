331f7ce1d7c32b7cfc1f0af435fc6020c7b0b6ab
brunoleox <bruno_leonardosilva@hotmail.com>
2021-10-13 21:14:47 -0400

    Componente de dialogo de confirmação

---
+++ frontend/src/components/confirmationDialog/index.jsx
@@ -0,0 +1,48 @@
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function ConfirmationDialog(props) {
+    const {
+      children,
+      open,
+      title,
+      cancelButtonText,
+      confirmButtonText,
+      onClose,
+      ...other
+    } = props;
+  
+    const handleCancel = () => {
+      onClose('cancel');
+    };
+  
+    const handleOk = () => {
+      onClose('ok');
+    };
+  
+    return (
+      <Dialog
+        disableBackdropClick
+        disableEscapeKeyDown
+        maxWidth="xs"
+        aria-labelledby="confirmation-dialog-title"
+        open={open}
+        {...other}
+      >
+        <DialogTitle id="confirmation-dialog-title">{title}</DialogTitle>
+        <DialogContent dividers>{children}</DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleCancel} color="primary">
+            {cancelButtonText || 'Cancelar'}
+          </Button>
+          <Button onClick={handleOk} color="primary">
+            {confirmButtonText || 'Ok'}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
