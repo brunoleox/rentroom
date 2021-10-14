f3e954f507dd8d2e8268cc66b12345d13683fe03
brunoleox <bruno_leonardosilva@hotmail.com>
2021-10-13 21:14:24 -0400

    Ajustes de rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Layout from "./layout";
+import Blocks from "./modules/blocks";
+import Rooms from "./modules/rooms";
 
 function App() {
   return (
-    <>
-      <Layout />
-    </>
+
+    <Layout>
+      <Switch>
+        <Route path="/rooms">
+          <Rooms />
+        </Route>
+        <Route path="/blocks">
+          <Blocks />
+        </Route>
+        <Route path="/account">
+          <Blocks />
+        </Route>
+        <Route path="/logout">
+          <Blocks />
+        </Route>
+      </Switch>
+    </Layout>
+
   );
 }
 
